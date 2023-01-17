02c2b04431f8572684113d69c1de6c11428f59c3
Xurshid <sotiboldiyevxurshid21@gmail.com>
2023-01-17 23:30:54 +0500

    ff

--- src/componet/Section2.jsx
+++ src/componet/Section2.jsx
@@ -46,6 +46,41 @@ export default function Section2() {
               </div>
             </div>
           </div>
+
+<div>
+<div className="col-4">
+            <div className="section2_div">
+              <div className="div">
+                <img src={pizza1} alt="" />
+                <h1 className="section_1">Blaze Pizza</h1>
+                <div className="section2_div_1">
+                  <h1 className="section2_soat">11.00 AM - 10.00 PM</h1>
+                  <button className="btn_olish">olish</button>
+                </div>
+              </div>
+
+              <div className="div2">
+                <img src={pizza2} alt="" />
+                <h1 className="section_2">Pizza Ranch</h1>
+                <div className="section2_div_1">
+                  <h1 className="section2_soat">11.00 AM - 10.00 PM</h1>
+                  <button className="btn_olish">olish</button>
+                </div>
+              </div>
+
+              <div className="div3">
+                <img src={pizza3} alt="" />
+                <h1 className="section_3">Dion’s Pizza Hut</h1>
+                <div className="section2_div_1">
+                  <h1 className="section2_soat">11.00 AM - 10.00 PM</h1>
+                  <button className="btn_olish">olish</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+</div>
+          
         </div>
       </div>
     </>
